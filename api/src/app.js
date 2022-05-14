ad37bd0ec660c909fa58e87f94acd632760cd7c2
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-05-14 18:05:20 -0500

    make it better

--- api/src/app.js
+++ api/src/app.js
@@ -1,21 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
-const cors = require("cors");
 
 require("./db.js");
 
 const server = express();
 
 server.name = "API";
+
 server.use(express.urlencoded({ extended: true, limit: "50mb" }));
 server.use(express.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
@@ -25,6 +25,7 @@ server.use((req, res, next) => {
   next();
 });
 server.use(cors());
+
 server.use("/", routes);
 
 // Error catching endware.
@@ -33,7 +34,7 @@ server.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
-  res.status(status).send({ message });
+  res.status(status).send(message);
 });
 
 module.exports = server;
