d8a0782c7cbb763df1a8e9a7edf2027e0719b0e8
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-05-15 13:29:06 -0500

    make it better

--- api/index.js
+++ api/index.js
@@ -18,27 +18,12 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
-const { conn, Type } = require("./src/db.js");
+const { conn } = require("./src/db.js");
 const model = require("../api/src/routes/helper/GetPreTypes.js");
 
-const dietLoader = async function () {
-  const dietsApi = await model.typesDit();
-  try {
-    dietsApi.forEach((e) =>
-      Type.findOrCreate({
-        where: {
-          name: e,
-        },
-      })
-    );
-  } catch (e) {
-    console.log(e);
-  }
-};
-
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT || 3001, () => {
-    dietLoader();
+  server.listen(process.env.PORT || 5000, () => {
+    model.typesDit();
 
     console.log(`(👍 ͡❛ ͜ʖ ͡❛)👍`);
   });
