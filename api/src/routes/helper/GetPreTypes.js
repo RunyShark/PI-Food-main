fa632b7597bc89c191e96d0719bad3c60e7c4b98
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-05-15 14:31:52 -0500

    make it better

--- api/src/routes/helper/GetPreTypes.js
+++ api/src/routes/helper/GetPreTypes.js
@@ -1,23 +1,16 @@
-const { KEY } = process.env;
+// const { KEY } = process.env;
 const axios = require("axios");
 const { Type } = require("../../db.js");
-
 const typesDit = async function () {
   try {
     const dietType = await axios.get(
-      `https:api.spoonacular.com/recipes/complexSearch?apiKey=d96f10caa842427da7a35a13eb53d2d9&number=100&addRecipeInformation=true`
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=9f3302f953754dfd81965c6448364ab7&number=100&addRecipeInformation=true`
     );
     const typ = await dietType.data.results.map((e) => e.diets).flat(1);
     let uno = [...new Set(typ)];
     console.log("types precargadas corectamente");
     console.log(uno);
-    uno.forEach((e) =>
-      Type.findOrCreate({
-        where: {
-          name: e,
-        },
-      })
-    );
+    uno.forEach((e) => Type.create({ name: e }));
   } catch (error) {
     console.log(error);
   }
