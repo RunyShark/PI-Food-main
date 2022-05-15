9d55849ed012799fb8c9e3bbf7adddd2caa2d40f
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-05-15 13:27:35 -0500

    make it better

--- api/src/routes/helper/GetPreTypes.js
+++ api/src/routes/helper/GetPreTypes.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const typesDit = async function () {
   try {
     const dietType = await axios.get(
-      `https:api.spoonacular.com/recipes/complexSearch?apiKey=06713865e5b04e40a2b774024d19b58f&number=50&addRecipeInformation=true`
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=06713865e5b04e40a2b774024d19b58f&number=60&addRecipeInformation=true`
     );
     const typ = await dietType.data.results.map((e) => e.diets).flat(1);
     return [...new Set(typ)];
