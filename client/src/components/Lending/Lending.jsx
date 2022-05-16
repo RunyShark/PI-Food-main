518ff412a9bcb7efb4ec6eac65804bb4062a6465
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-05-16 10:34:35 -0500

    css

--- client/src/components/Lending/Lending.jsx
+++ client/src/components/Lending/Lending.jsx
@@ -1,13 +1,49 @@
 import { Link } from "react-router-dom";
+import "./lending.css";
+import imgf from "../../helpers/Img/841be8b857448d1e2d59637111a8b4fa-logotipo-de-ropa-de-gorro-de-chef.png";
 
 const Lending = () => {
   return (
     <>
-      <div className="lending-dad">
-        <h1>CooK-BooK 🥕</h1>
-        <Link to={`/home`}>
-          <button>Bienvenido al app</button>
-        </Link>
+      <div className="lendin-full">
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className="lending-dad">
+          <div>
+            <button>
+              <Link to={`/home`}>Bienvenido a CooK-BooK 🥕</Link>
+            </button>
+          </div>
+          <img src={imgf} alt="hola" />
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
       </div>
     </>
   );
