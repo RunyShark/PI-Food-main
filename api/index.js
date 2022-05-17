6fcad6af55fd5b88e15014bdca7bbc17545f3a28
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-05-17 16:52:39 -0500

    make it better

--- api/index.js
+++ api/index.js
@@ -41,9 +41,9 @@ const typesDit = async function () {
   }
 };
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT || 5000, () => {
-    //typesDit();
+    typesDit();
     console.log(`(👍 ͡❛ ͜ʖ ͡❛)👍`);
   });
 });
