ed19ace3c15290f7911d966ac2b49dde23b6335f
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-05-21 00:05:39 -0500

    100%

--- client/src/Pages/Module/DetailsName/DetailsName.jsx
+++ client/src/Pages/Module/DetailsName/DetailsName.jsx
@@ -1,27 +1,46 @@
 import { useSelector } from "react-redux";
 import CardsName from "../CardsName/CardsName";
-
+import "./detailsName.css";
 const DetaisName = () => {
   const { allDataName } = useSelector((state) => state);
 
   return (
     <>
-      {allDataName.map((e) => {
-        return (
-          <div>
-            <CardsName
-              key={e.id}
-              name={e.name}
-              lvl={e.lvl}
-              img={e.img}
-              Types={e.Types}
-              detalis={e.detalis}
-              step={e.step}
-              id={e.id}
-            />
-          </div>
-        );
-      })}
+      {allDataName.length === 0 ? (
+        <>
+          <br />
+          <br />
+          <br />
+          <div class="classic-1"></div>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        </>
+      ) : (
+        allDataName.map((e) => {
+          return (
+            <div>
+              <CardsName
+                key={e.id}
+                name={e.name}
+                lvl={e.lvl}
+                img={e.img}
+                Types={e.Types}
+                detalis={e.detalis}
+                step={e.step}
+                id={e.id}
+              />
+            </div>
+          );
+        })
+      )}
     </>
   );
 };
